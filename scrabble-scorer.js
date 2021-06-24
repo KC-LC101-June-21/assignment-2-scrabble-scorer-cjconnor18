11c6fcc4218f2c247384c5e6387ff505de9db5f3
Carol Connor <45924187+cjconnor18@users.noreply.github.com>
2021-06-24 15:35:23 +0000

    B Add and Organize Scoring Algorithms

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,25 +33,78 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+  console.log("Let's play some scrabble!");
+  let currentWord = input.question("Enter a word:");
+  // let score = oldScrabbleScorer(currentWord);
+  // console.log(score);
+
+  return currentWord;
 };
 
-let simpleScore;
+let simpleScore = function (word) {
+  return word.length;
+};
 
-let vowelBonusScore;
+let vowelBonusScore = function (word) {
+  let score = 0;
+  for(let i = 0; i < word.length; i++) {
+    if(word[i].toLowerCase() === 'a'||
+    word[i].toLowerCase() === 'e'||
+    word[i].toLowerCase() === 'i'||
+    word[i].toLowerCase() === 'o'||
+    word[i].toLowerCase() === 'u') {
+      score += 3;
+    } else {
+      score += 1;
+    }
+  }
+  return score;
+};
 
 let scrabbleScore;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  {
+    name : "Simple Score",
+    description : "Each letter is worth 1 point", 
+    scoreFunction : simpleScore
+  }, 
+  {
+    name : "Vowel Bonus Score",
+    description : "Vowels are 3 pts, consonants are 1 pt.",
+    scoreFunction : vowelBonusScore
+  }, 
+  {
+    name: "Scrabble Score",
+    description: "The traditional scoring algorithm.",
+    scoreFunction : oldScrabbleScorer
+  }
+];
 
-function scorerPrompt() {}
+function scorerPrompt() {
+  console.log("Which scorer would you like to use? ");
+  for(let i = 0; i < scoringAlgorithms.length; i++) {
+    console.log(`${i} : ${scoringAlgorithms[i].name} - ${scoringAlgorithms[i].description}`);
+  }
+
+  let chosenIndex = input.question("Which number do you choose?");
+  while(Number(chosenIndex) < 0 || 
+      Number(chosenIndex) >= 3 ||
+      isNaN(chosenIndex)) {
+    chosenIndex = input.question("Invalid: Please choose 0 - 2: ");
+  }
+  return scoringAlgorithms[Number(chosenIndex)];
+}
 
 function transform() {};
 
 let newPointStructure;
 
 function runProgram() {
-   initialPrompt();
+   let currentWord = initialPrompt();
+   let scorerObject = scorerPrompt();
+   let score = scorerObject.scoreFunction(currentWord);
+   console.log(`Users score for ${currentWord} is ${score}`);
    
 }
 
